992e5bb71cf77404920b1af22f3cfae8da79517e
Никита Абакумов <nabakumov@mt.1tv.ru>
2021-02-12 12:58:15 +0300

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const app = document.querySelector('.box1')
+
+document.addEventListener('click', (ev) => {
+  ev.preventDefault()
+})
+
+class Car {
+  constructor() {}
+
+  get() {
+    setTimeout(() => {
+      const obj = {}
+    })
+  }
+}
